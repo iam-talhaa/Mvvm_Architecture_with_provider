40a12cdb504aba442ac4aeb1ed9965bbb422d20f
iam-talhaa <tahakhan4141@gmail.com>
2025-02-10 19:27:52 +0500

    Exception addded and Puckage installed

---
+++ lib/Data/App_Exception.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppException implements Exception {
+  final _messages;
+  final _prefix;
+
+  AppException([this._messages, this._prefix]);
+
+  String toString() {
+    return '$_messages $_prefix';
+  }
+}
+
+class unAuthorizedException extends AppException {
+  unAuthorizedException([String? message])
+      : super(message, "Un Aurthorized Acces");
+}
+
+class BadRequestException extends AppException {
+  BadRequestException([String? message]) : super(message, "Invalid Request");
+}
+
+class FetchDataException extends AppException {
+  FetchDataException([String? message])
+      : super(message, "Error During Communication");
+}
